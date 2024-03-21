2c252bd20dacb1ed197745b4eec56be4679559d8
Someone0813 <162572385+Someone0813@users.noreply.github.com>
2024-03-21 11:56:36 +0500

    Add files via upload

---
+++ homework7.2/src/App.jsx
@@ -0,0 +1,417 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="NavBar">
+        <img src="../public/NavIcon.svg" alt="NavIcon" className="NavIcon" />
+        <div className="NavNavigate">
+          <img
+            src="../public/NavNavigate1.svg"
+            alt="NavNavigate"
+            className="NavNavigateImgs"
+          />
+          <img
+            src="../public/NavNavigate2.svg"
+            alt="NavNavigate"
+            className="NavNavigateImgs"
+          />
+          <img
+            src="../public/NavNavigate3.svg"
+            alt="NavNavigate"
+            className="NavNavigateImgs"
+          />
+          <img
+            src="../public/NavNavigate4.svg"
+            alt="NavNavigate"
+            className="NavNavigateImgs"
+          />
+        </div>
+        <img
+          src="../public/AccountIcon.svg"
+          alt="AccountIcon"
+          className="AccountIcon"
+        />
+      </div>
+      <main>
+        <div className="Search">
+          <img src="../public/SearchIcon.svg" alt="SearchIcon" />
+          <input type="text" placeholder="Search for movies or TV series" />
+        </div>
+        <div className="Trending">
+          <p>Trending</p>
+          <span>
+            <div className="TrendCard1">
+              <img src="../public/CardSave.svg" alt="CardSave" />
+              <h1>2019 . Movie . PG</h1>
+              <h2>Beyond Earth</h2>
+            </div>
+            <div className="TrendCard2">
+              <img src="../public/CardSave.svg" alt="CardSave" />
+              <h1>2021 . Movie . PG</h1>
+              <h2>Bottom Gear</h2>
+            </div>
+            <div className="TrendCard3">
+              <img src="../public/CardSave.svg" alt="CardSave" />
+              <h1>2019 . TV Series . E</h1>
+              <h2>Bottom Gear</h2>
+            </div>
+          </span>
+        </div>
+        <div className="ForMe">
+          <p>Recommended for you</p>
+          <span>
+            <div>
+              <img
+                src="../public/MainCard (1).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . PG</h1>
+              <h2>The Great Lands</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (2).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . TV Series . PG</h1>
+              <h2>The Diary</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (3).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2017 . Movie . 18+</h1>
+              <h2>Earth’s Untouched</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (4).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . E</h1>
+              <h2>No Land Beyond</h2>
+            </div>
+          </span>
+          <span>
+            <div>
+              <img
+                src="../public/MainCard (5).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . PG</h1>
+              <h2>The Great Lands</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (6).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . TV Series . PG</h1>
+              <h2>The Diary</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (7).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2017 . Movie . 18+</h1>
+              <h2>Earth’s Untouched</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (8).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . E</h1>
+              <h2>No Land Beyond</h2>
+            </div>
+          </span>
+          <span>
+            <div>
+              <img
+                src="../public/MainCard (9).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . PG</h1>
+              <h2>The Great Lands</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (10).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . TV Series . PG</h1>
+              <h2>The Diary</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (11).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2017 . Movie . 18+</h1>
+              <h2>Earth’s Untouched</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (12).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . E</h1>
+              <h2>No Land Beyond</h2>
+            </div>
+          </span>
+          <span>
+            <div>
+              <img
+                src="../public/MainCard (13).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . PG</h1>
+              <h2>The Great Lands</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (14).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . TV Series . PG</h1>
+              <h2>The Diary</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (15).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2017 . Movie . 18+</h1>
+              <h2>Earth’s Untouched</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (16).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . E</h1>
+              <h2>No Land Beyond</h2>
+            </div>
+          </span>
+          <span>
+            <div>
+              <img
+                src="../public/MainCard (17).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . PG</h1>
+              <h2>The Great Lands</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (18).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . TV Series . PG</h1>
+              <h2>The Diary</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (19).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2017 . Movie . 18+</h1>
+              <h2>Earth’s Untouched</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (20).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . E</h1>
+              <h2>No Land Beyond</h2>
+            </div>
+          </span>
+          <span>
+            <div>
+              <img
+                src="../public/MainCard (21).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . PG</h1>
+              <h2>The Great Lands</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (22).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . TV Series . PG</h1>
+              <h2>The Diary</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (23).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2017 . Movie . 18+</h1>
+              <h2>Earth’s Untouched</h2>
+            </div>
+            <div>
+              <img
+                src="../public/MainCard (24).svg"
+                alt="MainCards"
+                className="MainCard"
+              />
+              <img
+                src="../public/CardSave.svg"
+                alt="CardSave"
+                className="CardSave"
+              />
+              <h1>2019 . Movie . E</h1>
+              <h2>No Land Beyond</h2>
+            </div>
+          </span>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
